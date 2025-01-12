b6dfeb84f2252b09d8ffc6b6e1a7744974c96e58
tosaken1116 <nagato20021122@gmail.com>
2025-01-12 15:34:58 +0900

    fix: change feature layout on small width

--- src/components/Feature/index.tsx
+++ src/components/Feature/index.tsx
@@ -2,7 +2,7 @@ import { PDTP_FEATURES } from "../../constants";
 
 export const Feature = () => {
 	return (
-		<ul className="grid grid-cols-3 gap-3">
+		<ul className="grid md:grid-cols-3 grid-cols-1 gap-3">
 			{PDTP_FEATURES.map((feature) => {
 				return (
 					<li
