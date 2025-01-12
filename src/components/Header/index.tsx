d1b1de8eec996014ee4e42680f266040e89b088b
tosaken1116 <nagato20021122@gmail.com>
2025-01-12 13:43:57 +0900

    feat: apply pdtp logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import { Z_INDEX } from "../../constants";
 import { GithubIcon } from "../icons/Github";
+import { PDTPLogo } from "../icons/Pdtp";
 
 export const Header = () => {
 	return (
@@ -12,16 +13,19 @@ export const Header = () => {
 		>
 			<div className="flex items-center px-4">
 				<div className=" w-full">
-					<h1 className="text-2xl font-bold">
-						<span>P</span>
-						<span className=" text-sm">ortable</span>
-						<span> D</span>
-						<span className=" text-sm">ocument</span>
-						<span> T</span>
-						<span className=" text-sm">ransfer</span>
-						<span> P</span>
-						<span className=" text-sm">rotocol</span>
-					</h1>
+					<span className="flex flex-row gap-1 items-center">
+						<PDTPLogo />
+						<h1 className="text-2xl font-bold">
+							<span>P</span>
+							<span className=" text-sm">ortable</span>
+							<span> D</span>
+							<span className=" text-sm">ocument</span>
+							<span> T</span>
+							<span className=" text-sm">ransfer</span>
+							<span> P</span>
+							<span className=" text-sm">rotocol</span>
+						</h1>
+					</span>
 				</div>
 				<a href="https://github.com/pdtp-workbench">
 					<GithubIcon />
